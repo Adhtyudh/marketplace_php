c290963162d38a8edd3d2bbb07a1837b99ca77df
Adhitya Yudha <adhtyudh@gmail.com>
2022-03-26 19:03:43 +0700

    update db

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect('localhost', 'root', '', 'db_perpustakaan');
+$koneksi = mysqli_connect('localhost', 'root', '', 'marketplace');
 if (!$koneksi) {
     echo "gagal tehubung";
 }
